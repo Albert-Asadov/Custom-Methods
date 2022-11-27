b94a440177869a886b2a9460e294b58b749f7f2b
Albert-Asadov <alberma@code.edu.az>
2022-11-27 20:10:17 +0400

    First and Second Tasks

---
+++ Custom Methods/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+#region Custom Contains
+using Custom_Methods;
+
+string customContains = "Bu bir custom contains methudodur";
+
+Console.WriteLine(customContains.CustomContains("tains"));
+#endregion
+
+
+
+
+#region Custom Split
+string customSplit = "Bu bir custom slip methodudur";
+
+string[] arraystring = customSplit.CustomSplit();
+
+
+foreach (var item in arraystring)
+{
+
+    Console.WriteLine(item);
+
+}
+#endregion
